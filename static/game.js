267435d462cfd0618b1c0259a453668c16aafd65
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-03-23 20:44:52 +0100

    finalize single player

--- static/game.js
+++ static/game.js
@@ -1,66 +1,66 @@
-function handleCardClick(event){
+/**
+ * Single player version of the game. It just checks the current cards and adds classes to them.
+ * Every time 2 cards are flipped, it checks if they match:
+ * - If the cards match, it adds the class "matched" to them.
+ * - If the cards don't match, it removes the class "flipped" from them.
+ * 
+ * @param {Event} event 
+ * @returns {void}
+ */
+function handleCardClick(event) {
   const card = event.currentTarget;
-  if (card.classList.contains("flipped")){
-      return;
-  }
-  if (card.classList.contains("matched")){
-      return;
-  }
-  console.log({card});
-  const cards = document.querySelectorAll(".card");
-  console.log({cards});
-  
-  const flipped = document.querySelectorAll(".flipped"); 
-  console.log({flipped});
 
-  const pairFlipped = flipped.length === 2;
+  if (card.classList.contains("flipped")) {
+    return;
+  }
+  if (card.classList.contains("matched")) {
+    return;
+  }
 
-  
-  // if we have two flipped cards already, flip them back
-  if (pairFlipped){
-      const [first, second] = flipped;
-      if (first.innerHTML.trim() === second.innerHTML.trim()){
-          first.classList.add("matched");
-          second.classList.add("matched");
-      }
-      for (let card of flipped){
-          card.classList.remove("flipped");
-      }
+  const flipped = document.querySelectorAll(".flipped");
+
+  if (flipped.length === 2) {
+    const [first, second] = flipped;
+    if (first.innerHTML.trim() === second.innerHTML.trim()) {
+      first.classList.add("matched");
+      second.classList.add("matched");
+    }
+    for (let card of flipped) {
+      card.classList.remove("flipped");
+    }
   }
   card.classList.toggle("flipped");
-
-
-  
-        
-}
-function addCard(emoji){
-    const card = document.createElement("div");
-    card.className = "card";
-
-    const front = document.createElement("div");
-    front.className = "front";
-    front.innerHTML = "🎴";
- 
-    
-    const back = document.createElement("div");
-    back.className = "back";
-    back.innerHTML = emoji;
-  
-
-    card.appendChild(front);
-    card.appendChild(back);
-    
-    card.onclick = handleCardClick;
-    document.querySelector("#game").appendChild(card);
-    
 }
 
-function addCards(){
-    const emojis = ["🍑", "🍑", "🥑 ", "🥑", "🍓", "🍓", "🧄", "🧄", "🍄", "🍄", "🍆 ", "🍆 " ]
-    const shuffled = emojis.sort(() => Math.random() - 0.5);
-    for (let emoji of shuffled){
-        addCard(emoji);
-    }
+/**
+ * Adds a card to the game. 
+ * Cards contain both sides and a click event listener.
+ * @param {String} emoji - The emoji to be added to the card. Javascript does not have char type, so we use string instead. 
+ * @returns {HTMLDivElement} - The card element.
+ */
+function addCard(emoji) {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  const front = document.createElement("div");
+  front.className = "front";
+  front.innerHTML = "🎴";
+
+
+  const back = document.createElement("div");
+  back.className = "back";
+  back.innerHTML = emoji;
+
+  card.appendChild(front);
+  card.appendChild(back);
+
+  card.onclick = handleCardClick;
+  return card;
 }
 
-addCards();
+
+const emojis = ["🍑", "🍑", "🥑 ", "🥑", "🍓", "🍓", "🧄", "🧄", "🍄", "🍄", "🍆 ", "🍆 "]
+const shuffled = emojis.sort(() => Math.random() - 0.5);
+for (let emoji of shuffled) {
+  window.game.appendChild(addCard(emoji));
+}
