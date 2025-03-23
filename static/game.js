b5ad9f42016cdc9e91cbefdd8bb39416f695ca00
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-03-23 20:21:46 +0100

    save WIP

--- static/game.js
+++ static/game.js
@@ -1,10 +1,14 @@
+const state = {
+  try: 0,
+  lastCard: null,
+}
 function addCard(emoji){
     const card = document.createElement("div");
     card.className = "card";
 
     const front = document.createElement("div");
     front.className = "front";
-    front.innerHTML = "🃏";
+    front.innerHTML = "🎴";
     front.hidden = false;
     
     const back = document.createElement("div");
@@ -16,6 +20,31 @@ function addCard(emoji){
     card.appendChild(back);
     
     card.onclick = function(){
+
+        state.try++;
+        if (state.try === 1){
+            state.lastCard = card;
+        }
+
+        if (state.try === 2){
+            if (state.lastCard.lastChild.innerHTML === card.lastChild.innerHTML){
+                state.try = 0;
+                state.lastCard = null;
+            
+                card.classList.add("found");
+                lastCard?.classList.add("found");
+                return
+            }
+        }
+        
+        if (state.try == 3){
+            state.try = 0;
+            const cards = document.querySelectorAll(".card:not(.found)"); 
+            for (let card of cards){
+                card.firstChild.hidden = false;
+                card.lastChild.hidden = true;
+            }
+        }
         front.hidden = front.hidden ? false : true;
         back.hidden = back.hidden ? false : true;
     }
@@ -24,7 +53,7 @@ function addCard(emoji){
 }
 
 function addCards(){
-    const emojis = ["🍑", "🍑", "🥦", "🥦", "🍓", "🍓", "🧄", "🧄", "🍄", "🍄", "🍆 ", "🍆 " ]
+    const emojis = ["🍑", "🍑", "🥑 ", "🥑", "🍓", "🍓", "🧄", "🧄", "🍄", "🍄", "🍆 ", "🍆 " ]
     const shuffled = emojis.sort(() => Math.random() - 0.5);
     for (let emoji of shuffled){
         addCard(emoji);
