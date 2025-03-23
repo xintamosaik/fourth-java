950271fe0b1c70f3792e6749eca215f6cf77d714
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-03-23 19:54:41 +0100

    add shuffled emojis
    Co-authored-by: med4kat <med4kat@users.noreply.github.com>

--- static/game.js
+++ static/game.js
@@ -1,26 +1,34 @@
+function addCard(emoji){
+    const card = document.createElement("div");
+    card.className = "card";
 
-const ctx = window.game.getContext("2d");
-ctx.font = "32px Courier New"
-ctx.fillStyle = "#0F0"
+    const front = document.createElement("div");
+    front.className = "front";
+    front.innerHTML = "🃏";
+    front.hidden = false;
+    
+    const back = document.createElement("div");
+    back.className = "back";
+    back.innerHTML = emoji;
+    back.hidden = true;
 
-function createImage({ width = 32, height = 32, r = 255, g = 255, b = 255, a = 255 }) {
-  const imgData = ctx.createImageData(width, height);
-  for (let i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i + 0] = r;
-    imgData.data[i + 1] = g;
-    imgData.data[i + 2] = b;
-    imgData.data[i + 3] = a;
-  }
-  return imgData
+    card.appendChild(front);
+    card.appendChild(back);
+    
+    card.onclick = function(){
+        front.hidden = front.hidden ? false : true;
+        back.hidden = back.hidden ? false : true;
+    }
+    document.querySelector("#game").appendChild(card);
+    
 }
 
-const sixtyFour = { width: 64, height: 64 }
-const red = { r: 255, g: 0, b: 0 }
-const exampleImage = createImage({ ...sixtyFour, ...red })
-
-ctx.putImageData(exampleImage, 10, 10);
-async function updateData() {
-  const response = await fetch('/update');
-  const data = await response.json();
-  document.getElementById('output').textContent = data.message;
+function addCards(){
+    const emojis = ["🍑", "🍑", "🥦", "🥦", "🍓", "🍓", "🧄", "🧄", "🍄", "🍄", "🍆 ", "🍆 " ]
+    const shuffled = emojis.sort(() => Math.random() - 0.5);
+    for (let emoji of shuffled){
+        addCard(emoji);
+    }
 }
+
+addCards();
